9e2bb616dc3472dd0577b2e5763a73560042067f
xtineroq <mcroque89@gmail.com>
2020-05-31 14:19:48 +1000

    created htmls, array of questions and some css

---
+++ questions.js
@@ -0,0 +1,29 @@
+// Array of questions, choices and answers
+var questionsArr = [
+    {
+      question: "Who invented Javascript?",
+      choices: ["Mark Zuckerberg", "Bill Gates", "Brendan Eich", "Douglas Crockford"],
+      answer: "Brendan Eich"
+    },
+    {
+      question: "Inside which HTML element do we put the JavaScript?",
+      choices: ["<scripting>", "<js>", "<script>", "<javascript>"],
+      answer: "<script>"
+    },
+    {
+      question: "How do you round the number 7.25, to the nearest integer?",
+      choices: ["Math.round(7.25)", "Math.rnd(7.25)", "round(7.25)", "rnd(7.25)"],
+      answer: "Math.round(7.25)"
+    },
+    {
+      question: "Which operator is used to assign a value to a variable?",
+      choices: ["-", "x", "+", "="],
+      answer: "="
+    },
+    {
+      question: "How can you add a comment in a JavaScript?",
+      choices: ["<!-- comment -->", "/* comment */", "// comment", "* comment"],
+      answer: "// comment"
+    }
+  ];
+  
