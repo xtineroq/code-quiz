950a8ab050b39344ebbdde0b1c2b5e704a98b3a4
xtineroq <mcroque89@gmail.com>
2020-06-06 23:30:56 +1000

    highscore, additional css, and updated readme

--- README.md
+++ README.md
@@ -1,2 +1,35 @@
 # cr-hw4-code-quiz
 Monash University Bootcamp Assignment - Code Quiz
+
+```
+This is a timed quiz on JavaScript fundamentals that stores high scores.
+```
+## Required:
+```
+When the Start button is clicked:
+* The timer starts and the user is presented with a question
+* The user would have to choose the answer from the clickable buttons on the screen
+
+When the user selects an answer:
+* The user will be prompted whether the answer is correct or wrong
+* If the user answers correctly, the user will be presented with another question
+* If the user's guess is incorrect, 10 seconds will be subtracted from the time
+* After a selection has been made, it will then move to the next question
+
+When all questions are answered or the timer reaches 0:
+* The game will end
+
+When the game is over
+* The user will be required to enter their initials
+* The user can then click the Submit button to save the score
+```
+## Additional:
+```
+* Buttons are highlighted when hovering
+* Wrong or Correct prompt disappears after a second
+```
+![start-screen](assets/start-screen.png)
+![question-screen](assets/question-screen.png)
+
+URL of deployed application:
+https://xtineroq.github.io/cr-hw4-code-quiz/
--- script.js
+++ script.js
@@ -28,8 +28,8 @@ $(document).ready(function() {
         $("#qTitle").html(loopQuestions.question);
         $("#qChoices").html("");
 
-        loopQuestions.choices.forEach(function(choice) {
-            $(".qChoices").append("<div><button>" + choice + "</button></div>");
+        loopQuestions.choices.forEach(function(selection) {
+            $(".qChoices").append("<div><button>" + selection + "</button></div>")
             $(".qChoices").click(revealAnswer);
         });
 
@@ -39,7 +39,7 @@ $(document).ready(function() {
     function revealAnswer() {
 
         // Compare user answer vs result
-        if ($(this).val() !== questionsArr[qcIndex].answer) {
+        if (typeof $(this)[qcIndex] !== questionsArr[qcIndex].answer) {
 
             timer -= 10;
             if (timer < 0) {
@@ -54,10 +54,10 @@ $(document).ready(function() {
             $("#result").text("Correct!");
         }
 
-        $("#result").addClass("result");
+        $(".result").removeClass("hide");
         setTimeout(function() {
-            $("#result").addClass("result hide");
-        }, 1000);
+            $(".result").addClass("hide");
+        }, 500);
 
         // Next question
         qcIndex++;
@@ -89,5 +89,51 @@ $(document).ready(function() {
         }
     }
 
+    // Save user initials and scores
+    $("#submitBtn").on("click", function saveScore() {
+        var userInitials = $("#userInit").val().trim();
+      
+        if (userInitials === null) {
+            $("#userInit").attr("placeholder", "Please enter your initials")
+        } else {
+          var highScores = JSON.parse(window.localStorage.getItem("highscores")) || [];
+      
+          var userScore = {
+            score: time,
+            userInitials: userInitials
+          };
+      
+          highScores.push(userScore);
+          window.localStorage.setItem("highscores", JSON.stringify(highScores));
+      
+          window.location.href = "highscores.html";
+        }
+    });
 
+    // If the user pressed enter to save
+    $("#userInit").keyup(function enterKey(event) {
+        if (event.key === "Enter") {
+          saveScore();
+        }
+    });
+
+    function showHighScores() {
+        var userHighScores = JSON.parse(window.localStorage.getItem("highScores")) || [];
+      
+        userHighScores.sort(function(x, y) {
+          return x.hScore - y.hScore;
+        });
+      
+        userHighScores.forEach(function(hScore) {
+          var scoreList = $("#highScores").text("<li>" + hScore.initials + " : " + hScore.hScore + "</li>");
+          $("#highScores").append(scoreList);
+        });
+      }
+      
+      $("#clearHS").on("click", function clearSavedScores() {
+        window.localStorage.removeItem("highScores");
+        window.location.reload();
+      });
+      
+      showHighScores();
 });
